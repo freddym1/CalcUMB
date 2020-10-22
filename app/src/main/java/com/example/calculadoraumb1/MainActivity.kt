39b0602cff404970c5019557c3ff91efa5882b72
je34429 <je34429@gmail.com>
2020-10-22 10:56:08 -0500

    se añade funcion de limpiado de display y se añade condicional para eliminar el cero inicial cuando se presiona una tecla

--- app/src/main/java/com/example/calculadoraumb1/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraumb1/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var num1: Double = 0.0
+    private var num2: Double = 0.0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,28 @@ class MainActivity : AppCompatActivity() {
         ceroBtn.setOnClickListener { numberPressed("0") }
         puntoBtn.setOnClickListener { numberPressed(".") }
 
+        clearBtn.setOnClickListener { resetAll() }
+
+
+
+
+    }
+
+
+    //Función cuando se presiona clear
+    private fun resetAll(){
+        resultadoText.text = "0"
+        num1 = 0.0
+        num2 = 0.0
     }
 
     //Función cuando se presiona un número
     private fun numberPressed(num: String){
+        if(resultadoText.text == "0" && num != ".") {
+            resultadoText.text = "$num"
+        } else {
             resultadoText.text = "${resultadoText.text}$num"
+        }
     }
 
 }
